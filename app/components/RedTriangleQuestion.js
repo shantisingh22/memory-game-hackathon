b292a9ee6bf6e2a6f838d48bffc28f168de9fd3e
Rani Singh <singhrani6792@gmail.com>
2025-06-30 23:52:30 +0530

    complete trai lgame

--- app/components/RedTriangleQuestion.js
+++ app/components/RedTriangleQuestion.js
@@ -1,5 +1,4 @@
 
-
 import React, { useState, useEffect } from "react";
 
 export default function RedTriangleQuestion({ image, hasRedTriangle, onAnswer }) {
--- app/components/homepage.js
+++ app/components/homepage.js
@@ -1,5 +1,6 @@
 
 "use client";
+
 import React, { useState } from "react";
 import Startpage from "./Startpage";
 import ParticipantInfo from "./ParticipantInfo";
